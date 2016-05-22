780a7b53992ca85a0ff7df49b89523155ac3f939
Matus <peciar.matus@gmail.com>
2016-05-22 17:58:18 +0800

    Before cleaning

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+var gulp   = require('gulp');
+var clean  = require('gulp-rimraf');
+var jshint = require('gulp-jshint');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var ngmin  = require('gulp-ng-annotate');
+var sourcemaps = require('gulp-sourcemaps');
+
+var sources = [
+  'src/ngDepth.directive.js'
+];
+
+var targets = 'dist/angular-parallax.{js,min.js,min.js.map}';
+
+gulp.task('clean', function() {
+  gulp.src(targets)
+    .pipe(clean());
+});
+
+gulp.task('lint', function() {
+  gulp.src(sources)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+gulp.task('compress', function() {
+  //Development version
+  gulp.src(sources)
+    .pipe(concat('ngDepth.directive.js', { newLine: '\n\n' }))
+    .pipe(ngmin())
+    .pipe(gulp.dest('./'));
+
+  //Minified version
+  gulp.src(sources)
+    .pipe(sourcemaps.init())
+      .pipe(concat('ngDepth.directive.min.js', { newLine: '\n\n' }))
+      .pipe(ngmin())
+      .pipe(uglify())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('default', ['lint', 'clean', 'compress']);
---
+++ src/ngDepth.directive.js
@@ -0,0 +1,157 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('ngDepth', [])
+        .provider('ngDepthConfig', function() {
+            this.config = {};
+            this.$get = function() {
+                return this;
+            };
+        })
+        .directive('ngDepth', ngDepth);
+
+    /** @ngInject */
+    function ngDepth($window) {
+        var directive = {
+            restrict: 'A',
+            scope: {
+                depthX: '=',
+                depthY: '=',
+                depthZ: '=',
+                rotateZ: '='
+            },
+            link: linkFunc,
+            controller: ngDepthCtrl,
+            controllerAs: 'vm'
+        };
+
+        return directive;
+
+        function linkFunc(scope, el, attr, vm) {
+
+            var elCenterX, elCenterY, elOffset,
+                doc = document.documentElement,
+                body = document.body;
+
+            initialize();
+            angular.element($window).bind("resize", initialize);
+
+            function initialize() {
+                if (vm.config.focalPoint === "parent") {
+                    angular.element(angular.element(el[0]).parent()[0]).bind("mousemove", updateElement);
+                } else {
+                    angular.element($window).bind("mousemove", updateElement);
+                }
+
+                elOffset = getOffset(el[0]);
+                elCenterX = elOffset.left + (el.prop('clientWidth') / 2);
+                elCenterY = elOffset.top + (el.prop('clientHeight') / 2);
+            }
+
+            function getOffset(el) {
+                var box = el.getBoundingClientRect();
+
+                var scrollTop = window.pageYOffset || doc.scrollTop || body.scrollTop;
+                var scrollLeft = window.pageXOffset || doc.scrollLeft || body.scrollLeft;
+
+                var clientTop = doc.clientTop || body.clientTop || 0;
+                var clientLeft = doc.clientLeft || body.clientLeft || 0;
+
+                var top = box.top + scrollTop - clientTop;
+                var left = box.left + scrollLeft - clientLeft;
+
+                return {
+                    top: top,
+                    left: left
+                };
+            }
+
+            function getMouseXY(event) {
+                var e = event || window.event;
+
+                var mouse = [];
+                mouse['x'] = e.clientX + (doc && doc.scrollLeft || body && body.scrollLeft || 0) - (doc.clientLeft || 0);
+                mouse['y'] = e.clientY + (doc && doc.scrollTop || body && body.scrollTop || 0) - (doc.clientTop || 0);
+                return mouse;
+            }
+
+            function updateElement(event) {
+                var mouse = getMouseXY(event);
+                var element = angular.element(el[0]);
+                var parent = element.parent()[0];
+                var x, y;
+
+                if (vm.config.focalPoint === 'elements') {
+                    x = elCenterX - mouse['x'];
+                    y = elCenterY - mouse['y'];
+                } else if (vm.config.focalPoint === 'window') {
+                    x = $window.innerHeight / 2 - mouse['x'];
+                    y = $window.innerHeight / 2 - mouse['y'];
+                } else if (vm.config.focalPoint === 'parent') {
+                    x = parent.offsetLeft + (angular.element(el[0]).parent()[0].clientWidth / 2) - mouse['x'];
+                    y = parent.offsetTop + (angular.element(el[0]).parent()[0].clientHeight / 2) - mouse['y'];
+                }
+
+                /* Collect ngDepth args supplied to the element */
+                var args = {};
+
+                args.depthX = scope.depthZ ? (x * scope.depthZ) : (x * scope.depthX);
+                args.depthY = scope.depthZ ? (y * scope.depthZ) : (y * scope.depthY);
+
+                args.rotateX = scope.rotateZ ? (x * scope.rotateZ) : (x * scope.depthX);
+                args.rotateY = scope.rotateZ ? (y * scope.rotateZ) : (y * scope.depthY);
+
+                // Apply global multiplicator to all elements
+                Object.keys(args).map(function(value, index) {
+                    args[value] = args[value] / vm.config.depthFactor;
+                });
+
+                transform(args);
+            }
+
+            function transform(args) {
+                var transformCSS = "";
+
+                if (scope.rotateZ) {
+                    transformCSS += 'perspective( 600px ) rotateY( ' + -args.rotateX + 'deg ) rotateX( ' + args.rotateY + 'deg )';
+                }
+                if (scope.depthZ) {
+                    transformCSS += 'translate(' + args.depthX + 'px, ' + args.depthY + 'px)';
+                }
+                if (scope.depthX && !scope.depthY && !scope.depthZ) {
+                    transformCSS += 'translateX(' + args.depthX + 'px)';
+                }
+                if (!scope.depthX && scope.depthY && !scope.depthZ) {
+                    transformCSS += 'translateY(' + args.depthY + 'px)';
+                }
+                if (scope.depthX && scope.depthY && !scope.depthZ) {
+                    transformCSS += 'translate(' + args.depthX + 'px, ' + args.depthY + 'px)';
+                }
+
+                el.css('transform', transformCSS);
+            }
+        }
+
+        /** @ngInject */
+        ngDepthCtrl.$inject = ['ngDepthConfig'];
+        function ngDepthCtrl(ngDepthConfig) {
+            var vm = this;
+            vm.config = {
+                'focalPoint': 'parent',
+                /* options: (elements, window, parent) */
+                'depthFactor': 10
+                    /* global multiplicator */
+            };
+
+            // Interpolate config settings
+            Object.keys(ngDepthConfig.config).map(function(value, index) {
+                vm.config[value] = ngDepthConfig.config[value];
+            });
+
+            return vm;
+        }
+
+    }
+
+})();
